480dba8396aa2bcd6d8cdded6b607ae3da9568fb
Alexander Tetervak <tetervak@sheridancollege.ca>
2021-09-16 11:08:41 -0400

    corrected the view binding in the activity

--- app/src/main/java/ca/tetervak/catmessage2/MainActivity.kt
+++ app/src/main/java/ca/tetervak/catmessage2/MainActivity.kt
@@ -6,15 +6,19 @@ import androidx.fragment.app.FragmentManager
 import ca.tetervak.catmessage2.model.Envelope
 import ca.tetervak.catmessage2.ui.InputFragment
 import ca.tetervak.catmessage2.ui.OutputFragment
+import ca.tetervak.catmessage2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), InputFragment.Listener, OutputFragment.Listener {
     companion object{
         const val INPUT_TO_OUTPUT = "Input_to_Output"
     }
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         if(savedInstanceState == null){
             supportFragmentManager.beginTransaction()
